3bd4a4f32e2e341d570b8451e128b2def081c7ff
Andrew Khadder <andrewkh15@gmail.com>
2024-05-29 17:26:02 -0700

    project riv

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,9 @@
 const Intro = () => {
   return (
     <div className="flex flex-col overflow-hidden h-fit w-full">
+      <div className="py-10 bg-kappa-orangesky">
+        <p className="text-center text-xl text-white font-bowlby lg:text-6xl">PROJECT RIV PRESENTS</p>
+      </div>
       <div className="h-full w-full lg:flex lg:justify-center lg:items-center z-10">
         <video
           className=" lg:inset-0 lg:object-cover lg:w-full lg:h-full"
